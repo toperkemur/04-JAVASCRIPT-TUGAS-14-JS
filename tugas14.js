3f2f848756122cde6d7c37c1cdd564c060c9bbe3
Christopher Andrew Kemur <christopher.kemur@gmail.com>
2020-04-29 18:16:16 +0700

    Initial Commit
    Menambahkan tugas14.js

---
+++ tugas14.js
@@ -0,0 +1,5 @@
+var x = 37 / 15;
+console.log(x);
+console.log("pembulatan round: ", Math.round(x));
+console.log("pembulatan ceil: ", Math.ceil(x));
+console.log("pembulatan floor: ", Math.floor(x));
